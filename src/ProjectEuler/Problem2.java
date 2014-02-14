dac773aeda28a7c45bd4277b4101465481e467fd
James Pfeiffer <jamesrpfeiffer@gmail.com>
2014-02-13 20:43:25 -0800

    Solved problem 2.

---
+++ src/ProjectEuler/Problem2.java
@@ -0,0 +1,19 @@
+package ProjectEuler;
+
+public class Problem2 {
+
+    public int solution(int max){
+        int prev, total, curr, temp;
+        total = 0;
+        prev = 0;
+        curr = 1;
+        while(curr < max){
+            if(curr%2 == 0) {
+                total += curr;
+            }
+            curr = curr + prev;
+            prev = curr - prev;
+        }
+        return total;
+    }
+}
--- src/ProjectEuler/Runner.java
+++ src/ProjectEuler/Runner.java
@@ -2,13 +2,17 @@ package ProjectEuler;
 
 public class Runner {
 
-    private static int problem = 1;
+    private static int problem = 2;
 
     public static void main(String[] args) {
         switch(problem) {
             case 1:
                 Problem1 problem1 = new Problem1();
                 System.out.println(Integer.toString(problem1.solution(3,5,1000)));
+                break;
+            case 2:
+                Problem2 problem2 = new Problem2();
+                System.out.println(Integer.toString(problem2.solution(4000000)));
                 break;
         }
     }
